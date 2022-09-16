b38c679b971d8fcdf2b28b00f4b88fb2dbae3a06
Anais <victor.bolivar@pucp.edu.pe>
2022-09-16 08:54:09 -0500

    Vista principal y activities creados

--- app/src/main/java/pe/edu/pucp/myapplication/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package pe.edu.pucp.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_computadora);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ComputadoraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void irTeclado(View view){
+        Intent intent = new Intent(this, TecladoActivity.class);
+        startActivity( intent );
+    }
+
+    public void irMouse(View view){
+        Intent intent = new Intent(this, MouseActivity.class);
+        startActivity( intent );
+    }
+
+    public void irMonitor(View view){
+        Intent intent = new Intent(this, MonitorActivity.class);
+        startActivity( intent );
+    }
+
+    public void irReporte(View view){
+        Intent intent = new Intent(this, ReporteActivity.class);
+        startActivity( intent );
     }
 }
