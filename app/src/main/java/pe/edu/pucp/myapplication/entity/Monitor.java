1e33e7b3ce11d81a1452b32a4bbc5bbaa6afae67
pieroefe <a20180524@pucp.edu.pe>
2022-09-18 22:08:39 -0500

    para jose

---
+++ app/src/main/java/pe/edu/pucp/myapplication/entity/Monitor.java
@@ -0,0 +1,64 @@
+package pe.edu.pucp.myapplication.entity;
+
+import java.io.Serializable;
+
+public class Monitor implements Serializable {
+
+    public String getActivo() {
+        return activo;
+    }
+
+    public void setActivo(String activo) {
+        this.activo = activo;
+    }
+
+    public int getPcActivo() {
+        return pcActivo;
+    }
+
+    public void setPcActivo(int pcActivo) {
+        this.pcActivo = pcActivo;
+    }
+
+    public int getMarca() {
+        return marca;
+    }
+
+    public void setMarca(int marca) {
+        this.marca = marca;
+    }
+
+    public int getPulgadas() {
+        return pulgadas;
+    }
+
+    public void setPulgadas(int pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    private String activo;
+    private int  pcActivo;
+    private int marca;
+    private int pulgadas;
+    private int ano;
+    private String modelo;
+
+
+
+}
