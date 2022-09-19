a6fb7fa0b0ffa2323aca17ff5c96df947e75d4c5
Stephania1807 <stephania.guzman@pucp.edu.pe>
2022-09-18 19:54:45 -0500

    push de una anais  >:(

--- app/src/main/java/pe/edu/pucp/myapplication/AgregarTecladoActivity.java
+++ app/src/main/java/pe/edu/pucp/myapplication/AgregarTecladoActivity.java
@@ -1,9 +1,56 @@
 package pe.edu.pucp.myapplication;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.edu.pucp.myapplication.entity.Computadora;
+import pe.edu.pucp.myapplication.entity.listaComputadoras;
 
 public class AgregarTecladoActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teclado_agregar);
 
+
+        //spinner PC activos
+
+        List<String> valuesSpinner = new ArrayList<>();
+        valuesSpinner.add(0,"PC Activo:");
+
+        for (Computadora comp: listaComputadoras.getListaComputadoras()){
+            valuesSpinner.add(comp.getActivo());
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,valuesSpinner);
+        Spinner spinner = findViewById(R.id.activo_pc);
+        spinner.setAdapter(arrayAdapter);
+
+
+        //spinner Marcas
+        String [] marcas={"Marca:","Asus","Lenovo","Msi","Razer","Microsoft","Logitech","VSG"};
+        ArrayAdapter<String> adapterMarcas= new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,marcas);
+        Spinner spinnerMarcas =findViewById(R.id.marca_teclado);
+        //pinnerMarcas.setPrompt("Marca");
+        spinnerMarcas.setAdapter(adapterMarcas);
+
+        //spinner Idioma
+        String [] idiomas={"Idioma:","Español Latam","Ingles","Frances","Italiano","Chino","Japones","Coreano"};
+        ArrayAdapter<String> adapterIdioma= new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,idiomas);
+        Spinner spinnerIdioma =findViewById(R.id.idioma_teclado);
+        spinnerIdioma.setAdapter(adapterIdioma);
+
+
+
+
+
+    }
 
 }
